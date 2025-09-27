ddb7c7c6b686118e77d73315cff5268e55aa9255
Alexey-creator <nival2008@mail.ru>
2025-09-27 17:15:40 +0300

    Шаг 4: Движок игр

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import greetUser from './cli.js';
+import runGreeting from './cli.js';
+import readlineSync from 'readline-sync';
 
 const runEngine = (description, generateRound) => {  
-  const userName = greetUser();
+  const userName = runGreeting();
+  console.log(description);
+  const roundsCount = 3;
+  for (let i = 0; i < roundsCount; i++) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const  userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+     console.log('Correct!');
+    } else {
+     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+     console.log(`Let's try again, ${userName}!`)
+     return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`)
 };
 
 export default runEngine;
